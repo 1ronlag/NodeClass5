5f59d6b4474b9217f608c693c1c325e20133535f
1ronlag <nico.saavedrap@gmail.com>
2023-01-14 20:35:35 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+const { reportRequest } = require("./src/middleware/logger");
+
+app.use(cors());
+app.use(reportRequest);
+app.use(express.json());
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Servidor encendido en puerto ${port}`);
+});
+
+
+module.exports = app;
